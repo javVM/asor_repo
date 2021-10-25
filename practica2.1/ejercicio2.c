e95234ddbf087288d6495ae68568516975bfd8ed
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2021-10-25 10:11:27 +0200

    Add files via upload

---
+++ practica2.1/ejercicio2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    
+   if(setuid(0) != 0){
+     printf("Se ha producido un error, con numero %d: %s\n", errno, strerror(errno));
+   }
+   else printf("Se ha cambiado el UID de forma satisfactoria.\n");
+   
+   return 1;
+}
+
