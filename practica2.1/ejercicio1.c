82ad1ce2a4a18c78695fbdf377777d4fd096edf3
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2021-10-25 09:59:40 +0200

    Create ejercicio1.c

---
+++ practica2.1/ejercicio1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+
+   if(setuid(0) != 0){
+     perror("Ha ocurrido un error" + );
+   }
+   else printf("Se ha cambiado el UID de forma satisfactoria.");
+   return 1;
+}
