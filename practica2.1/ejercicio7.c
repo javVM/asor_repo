40e8384318dcb12d5ac474aacd58c251704723c3
javVM <jverdemarin@gmail.com>
2021-10-27 00:43:19 +0200

    Ejercicio 7 actualizado

--- practica2.1/ejercicio7.c
+++ practica2.1/ejercicio7.c
@@ -8,8 +8,13 @@ int main() {
   long int maxLinks = pathconf(dir,_PC_LINK_MAX);
   long int maxRoute = pathconf(dir,_PC_PATH_MAX);
   long int maxName = pathconf(dir,_PC_NAME_MAX);
+  
+  if(maxLinks != -1) printf("El numero maximo de enlaces es: %li\n", maxLinks);
+  else printf("No hay numero maximo de enlaces.\n");
+  
+  if(maxRoute != -1) printf("La longitud maxima de ruta es: %li\n", maxRoute);
+  else printf("No hay longitud maxima de ruta.\n");
 
-  printf("EL numero maximo de enlaces es: %li\n", maxLinks);
-  printf("La longitud maxima de ruta es: %li\n", maxRoute);
-  printf("La longitud maxima del nombre de fichero es: %li\n", maxName);
+  if(maxName != -1) printf("La longitud maxima del nombre de fichero es: %li\n", maxName);
+  else printf("No hay longitud maxima  para los nombres de fichero\n");
 }
