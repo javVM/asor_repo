a6531ce71db04f84c72344680ae26c13a0f52826
javVM <jverdemarin@gmail.com>
2021-10-27 00:49:59 +0200

    Ejercicio 6 actualizado

--- practica2.1/ejercicio6.c
+++ practica2.1/ejercicio6.c
@@ -7,9 +7,16 @@ int main() {
  long int maxArguments = sysconf(_SC_ARG_MAX);
  long int maxChild = sysconf(_SC_CHILD_MAX);
  long int maxFiles = sysconf(_SC_OPEN_MAX);
- printf("La longitud maxima de los argumentos es: %li\n", maxArguments);
- printf("El maximo numero de hijos es: %li\n", maxChild);
- printf("El maximo numero de ficheros abiertos es: %li\n", maxFiles);
- return 0;
+
+ if(maxArguments != -1) printf("La longitud maxima de los argumentos es: %li\n", maxArguments);
+ else printf("Se ha procucido un error\n");
+
+ if(maxChild != -1) printf("El maximo numero de hijos es: %li\n", maxChild);
+ else printf("Se ha producido un error\n");
+
+ if(maxFiles != -1) printf("El maximo numero de ficheros abiertos es: %li\n", maxFiles);
+ else printf("Se ha producido un error\n");
+
+return 0;
 }
 
