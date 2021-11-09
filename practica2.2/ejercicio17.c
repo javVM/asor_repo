32c3c0eb9cfa4211f634f4608f6b72f4867bc717
javVM <jverdemarin@gmail.com>
2021-11-09 14:48:03 +0100

    Ejercicio17 Practica 2 actualizado

--- practica2.2/ejercicio17.c
+++ practica2.2/ejercicio17.c
@@ -10,21 +10,23 @@
 
 //Esta funcion se encarga de mostrar la informacion de cada elemento del directorio por pantalla
 //Devuelve true (1) en caso de ser un directorio y false (0) en caso contrario, asi solo se sumaran los tamanios en bytes de los ficheros
-int printFile(const char *  name, struct stat buff) {
+int printFile(const char * path, const char *  name, struct stat buff) {
+    int result = 0;
 
     if(S_ISLNK(buff.st_mode)){ //Si el fichero es un enlace simbolico
       char * b = malloc(buff.st_size + 1);
-      readlink(name, b,  buff.st_size);
+      readlink(path, b,  buff.st_size);
       printf("%s->%s\n", name,b);
       free(b);
     }
     else if (S_ISDIR(buff.st_mode)){ //Si es un directorio
          printf("%s/\n", name);
-         return 1;
+         result = 1;
     }
-    else if (access(name,X_OK) == 0) printf("%s*\n", name); //Solo necesitamos saber si se puede ejecutar, ya sabemos que existe de antemano
+    else if (access(path,X_OK) == 0) printf("%s*\n", name); //Solo necesitamos saber si se puede ejecutar, ya sabemos que existe de antemano
     else printf("%s\n", name); //Si es un fichero regular u otro no especificado
-    return 0;
+    
+    return result;
 }
 
 
@@ -38,8 +40,13 @@ int main(int argc, char * argv[]) {
    int isDir;
 
     while((content = readdir(directory)) != NULL) {
-       lstat(content->d_name, &buff);
-       isDir = printFile(content->d_name,buff);
+       char * fullpath = malloc(strlen(argv[1]) + strlen(content->d_name) + 2);
+       strcpy(fullpath, argv[1]);
+       strcat(fullpath, "/");
+       strcat(fullpath, content->d_name);
+       lstat(fullpath, &buff);
+       isDir = printFile(fullpath,content->d_name,buff);
+       free(fullpath);
        //si la funcion devuelve que no es un directorio sumamos el tamano de ese fichero
        if(isDir == 0) byteSize += buff.st_size;       
     }
