c5f535ebed61b0d9c0392abd3f3b51f2b11b21c7
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2021-11-22 10:45:00 +0100

    Create ejercicio2.c

---
+++ practica2.3/ejercicio2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sched.h>
+#include <string.h>
+
+char * getPolicy(int policy){
+  if(policy == SCHED_FIFO) return "SCHED_FIFO";
+  else if (policy == SCHED_RR) return "SCHED_RR";
+  else if (policy == SCHED_OTHER) return "SCHED_OTHER";
+}
+
+int main() {
+  
+  struct sched_param  p;
+  sched_getparam(0, &p);
+  int scheduler = sched_getscheduler(0);
+  printf("Politica de planificacion del proceso actual: %s\n", getPolicy(scheduler));
+  printf("Prioridad del proceso actual: %i\n", p.sched_priority);
+  printf("Prioridad maxima: %i\n", sched_get_priority_max(scheduler));
+  printf("Prioridad minima: %i\n", sched_get_priority_min(scheduler));
+
+}
