44419bf74fd5acde4b7c765873b15a10c50e3a8f
javVM <jverdemarin@gmail.com>
2021-11-24 01:12:43 +0100

    Ejercicio5 practica 3 actualizado: Directorio y max ficheros añadidos

--- practica2.3/ejercicio5.c
+++ practica2.3/ejercicio5.c
@@ -1,15 +1,27 @@
 //Javier Verde Marin
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#define PATH_MAX 4096
 
 int main() {
   
   pid_t pid = getpid();
-  
+  char * buffer; 
+  struct rlimit rlim; 
+
   printf("El el PID es: %i\n", getpid());
   printf("El PPID es: %i\n",getppid());
   printf("El PGID es: %i\n", getpgid(pid));
   printf("El SID es: %i\n", getsid(pid));
+  
+  if(getrlimit(RLIMIT_NOFILE, &rlim) != -1) printf("El maximo numero de ficheros que puede abrir el proceso es: %i\n", rlim.rlim_max);
+  else perror("Error al usar getrlimit()\n");
+
+  if(getcwd(buffer, PATH_MAX)) printf("El directorio de trabajo actual es: %s\n", buffer);  
+  else perror("Error al usar getcwd()\n");
 
   return 0;
 }
