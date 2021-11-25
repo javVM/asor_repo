6b9891d40e1f76f3ed791ec0f3cd852e3a63bd8b
javVM <jverdemarin@gmail.com>
2021-11-25 22:18:21 +0100

    Ejercicio12 Practica3 subido

---
+++ practica2.3/ejercicio12.c
@@ -0,0 +1,37 @@
+//Javier Verde Marin
+#include <stdio.h>
+#include <signal.h>
+
+volatile int numInt=0;
+volatile int numStp=0;
+
+static void handler(int signal)
+{
+  if(signal == SIGINT) numInt++;
+  else if(signal == SIGTSTP) numStp++;
+}
+
+int main() {
+
+ struct sigaction act;
+ act.sa_handler = handler;
+ sigemptyset(&act.sa_mask);
+ sigaddset(&act.sa_mask, SIGINT);
+ sigaddset(&act.sa_mask, SIGTSTP);
+ act.sa_flags = SA_RESTART;
+
+ sigaction(SIGINT, &act, NULL);
+ sigaction(SIGTSTP, &act, NULL);
+
+ sigset_t set;
+ sigemptyset(&set);
+
+ while(numInt + numStp < 10) {
+   sigsuspend(&set);
+ }
+ 
+ printf("Se han recibido %i señales de tipo SIGINT\n", numInt);
+ printf("Se han recibido %i señales de tipo SITSTP\n", numStp);
+
+ return 0;
+}
