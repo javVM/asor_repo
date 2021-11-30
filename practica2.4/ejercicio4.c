e7e51807c55187d75be6e8bcaa80404e5a579d7a
javVM <jverdemarin@gmail.com>
2021-11-30 20:43:32 +0100

    Ejercicio4 Practica4 Subido

---
+++ practica2.4/ejercicio4.c
@@ -0,0 +1,26 @@
+//Javier Verde Marín
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char * argv[]) {
+  
+ if(argc < 2){
+    perror("Se necesita un argumento\n");
+    return -1;
+ } 
+
+ int fd = open("tuberia", O_WRONLY); 
+
+ if(fd == -1) {
+    perror("Problema al abrir la tuberia con open()\n");
+    return -1;
+ }
+ 
+ write(fd, argv[1], strlen(argv[1])+1);
+
+ return 0;
+}
